6ee401956d3d8577e9e61dc0e080690b0a188217
gugahnstn <gustavonstn@hotmail.com>
2023-04-15 20:57:28 -0300

    movendo arquivos

--- source/controllers/index.js
+++
@@ -1,4 +0,0 @@
-export const requestIp = ('/', (request, response) => {
-  const ip = request.ip;
-  response.json({message: ip});
-});
--- source/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { requestIp } from "../controllers/index.js";
-import { Router } from "express";
-
-const router = Router();
-
-router.get('/', requestIp);
-
-export { router };

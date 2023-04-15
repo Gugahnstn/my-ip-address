104343083a3c7046bc0cb7725becaea33117dc80
gugahnstn <gustavonstn@hotmail.com>
2023-04-15 20:14:15 -0300

    feat: mudando a estrutura das pastas

---
+++ api/core/server.js
@@ -0,0 +1,26 @@
+import { app } from "./source/app.js";
+
+class DefaultPort {
+  constructor(value) {
+    this.value = value;
+  }
+
+  verifyPort() {
+    const port = parseInt(this.value, 10);
+
+    if (isNaN(port)) {
+      return this.value;
+    } else if (port >= 0) {
+      return port;
+    }
+
+    return false;
+  }
+}
+
+const PORT = new DefaultPort(process.env.PORT || 3000);
+
+app.listen(PORT.verifyPort(), (err) => {
+  if (err) console.log(err);
+  console.log(`App listening on port ${PORT.verifyPort()}`);
+});

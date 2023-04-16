241aa1b185eb90e51f2a06d76f51b244037cc991
gugahnstn <gustavonstn@hotmail.com>
2023-04-16 03:57:54 -0300

    feat: modificando o retorno da api

--- api/core/source/controllers/index.ts
+++ api/core/source/controllers/index.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export const requestIp = (request: Request, response: Response) => {
   const ip = request.ip;
-  response.json({message: ip});
+  response.json({query: ip});
 };

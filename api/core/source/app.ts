d2e08bcb1c45a6f4583ed7bdf3afa08a0d2eee4e
gugahnstn <gustavonstn@hotmail.com>
2023-04-25 02:04:23 -0300

    feat: configurando o cors

--- api/core/source/app.ts
+++ api/core/source/app.ts
@@ -3,10 +3,14 @@ import { router } from "./routes/index";
 import cors from 'cors';
 
 const app: Express = express();
+const corsOptions = {
+  origin: 'https://ipcats.bohr.io/',
+  optionsSuccessStatus: 200
+}
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(router);
-app.use(cors());
+app.use(cors(corsOptions));
 
 export { app };
--- api/core/source/routes/index.ts
+++ api/core/source/routes/index.ts
@@ -1,13 +1,8 @@
 import { requestIp } from "../controllers/index.js";
 import { Router } from "express";
-import cors from 'cors';
 
 const router = Router();
-const corsOptions = {
-  origin: 'https://ipcats.bohr.io/',
-  optionsSuccessStatus: 200
-}
 
-router.get('/', cors(corsOptions), requestIp);
+router.get('/', requestIp);
 
 export { router };

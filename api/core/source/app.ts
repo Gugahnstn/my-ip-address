dc43aeb29dd702e5dd4db6addd59605b153d4f69
gugahnstn <gustavonstn@hotmail.com>
2023-04-25 01:59:41 -0300

    feat: utilizando o cors na aplicação

--- api/core/source/app.ts
+++ api/core/source/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
 import { router } from "./routes/index";
+import cors from 'cors';
 
 const app: Express = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 export { app };
